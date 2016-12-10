9bf23fdd621ad7b34b528d36590a36f4603f084b
Hussein Mohammed <zidhussein@gmail.com>
2016-12-10 16:29:03 +0000

    Remove unnecesary semi colons

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,15 @@
-import gulp from 'gulp';
-import postcss from 'gulp-postcss';
-import cssImport from 'postcss-import';
-import report from 'postcss-reporter';
-import cssnext from 'postcss-cssnext';
-import del from 'del';
-import browserSync from 'browser-sync';
+import gulp from 'gulp'
+import postcss from 'gulp-postcss'
+import cssImport from 'postcss-import'
+import report from 'postcss-reporter'
+import cssnext from 'postcss-cssnext'
+import del from 'del'
+import browserSync from 'browser-sync'
 
-const bs = browserSync.create();
+const bs = browserSync.create()
 
 // Delete build directory
-gulp.task('clean', () => del('./build'));
+gulp.task('clean', () => del('./build'))
 
 // Serve files
 gulp.task('serve', ['build'], () => {
@@ -17,16 +17,16 @@ gulp.task('serve', ['build'], () => {
     server: {
       baseDir: './build'
     }
-  });
-  gulp.watch('./src/**/*.css', ['styles']);
-  gulp.watch('src/**/*.html', ['html']).on('change', bs.reload);
-});
+  })
+  gulp.watch('./src/**/*.css', ['styles'])
+  gulp.watch('src/**/*.html', ['html']).on('change', bs.reload)
+})
 
 // Move HTML files to build directory
 gulp.task('html', () => {
   return gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./build/'));
-});
+    .pipe(gulp.dest('./build/'))
+})
 
 // Run styles through postcss then move to build directory
 gulp.task('styles', () => {
@@ -34,14 +34,14 @@ gulp.task('styles', () => {
     cssImport(),
     report(),
     cssnext()
-  ];
+  ]
   return gulp.src('./src/**/*.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./build'))
-    .pipe(bs.stream());
-});
+    .pipe(bs.stream())
+})
 
 // Build
-gulp.task('build', ['html', 'styles']);
+gulp.task('build', ['html', 'styles'])
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build'])
